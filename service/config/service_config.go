cf91e00e204c5140a5c8695d8b87ec44c6e75200
Jacob <jmbarzee@gmail.com>
2021-04-22 12:31:38 -0600

    add ServicePort to service configuration tooling

--- service/config/service_config.go
+++ service/config/service_config.go
@@ -13,6 +13,7 @@ type ServiceConfig struct {
 	DominionPort int
 	DomainUUID   string
 	ServiceType  string
+	ServicePort  int
 }
 
 // DefaultServiceDialTimeout is the default time out for grpc dial operations
@@ -23,19 +24,27 @@ func FromEnv(serviceType string) (config ServiceConfig, err error) {
 	dominionIPString := os.Getenv("DOMINION_IP")
 	dominionIP := net.ParseIP(dominionIPString)
 
-	dominionPortString :=  os.Getenv("DOMINION_PORT")
+	dominionPortString := os.Getenv("DOMINION_PORT")
 	dominionPort64, err := strconv.ParseInt(dominionPortString, 0, 32)
 	if err != nil {
 		return ServiceConfig{}, err
 	}
 	dominionPort := int(dominionPort64)
 
-	domainUUID :=  os.Getenv("DOMAIN_UUID")
+	domainUUID := os.Getenv("DOMAIN_UUID")
+
+	servicePortString := os.Getenv("SERVICE_PORT")
+	servicePort64, err := strconv.ParseInt(servicePortString, 0, 32)
+	if err != nil {
+		return ServiceConfig{}, err
+	}
+	servicePort := int(servicePort64)
 
 	return ServiceConfig{
 		DominionIP:   dominionIP,
 		DominionPort: dominionPort,
 		DomainUUID:   domainUUID,
 		ServiceType:  serviceType,
+		ServicePort:  servicePort,
 	}, nil
 }
